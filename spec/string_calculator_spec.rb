2c228c096df7c3434a7e6d3c5cc3d4e790f5659d
Nimesh Nikum <nimesh.nikum@gmail.com>
2025-01-21 23:07:23 +0100

    Basic implementation of add method to sum multiple numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,5 +9,13 @@ RSpec.describe StringCalculator do
     it "returns 0 for an empty string" do
       expect(string_calculator.add("")).to eq(0)
     end
+
+    it "returns the same number if only one input is given" do
+      expect(string_calculator.add("1")).to eq(1)
+    end
+
+    it "returns the sum of two numbers separated by comma" do
+      expect(string_calculator.add("1,5")).to eq(6)
+    end
   end
 end
